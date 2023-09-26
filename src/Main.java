7fa00e1c281e46456b249779f2424d8a5314b2ea
unknown <ivannikovmax04@gmail.com>
2023-09-26 13:52:43 +0300

    Fixed formatting

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,14 @@ public class Main {
         int n = 7;
         int m = 9;
         System.out.println("StudentNum = 9\nO1 = '-'\nO2 = '-'\nC = 0\nj,i = 'int'");
-        System.out.println("C2="+C2+"\nC3="+C3+"\nC5="+C5+"\nC7="+C7);;
+        System.out.println("C2=" + C2 + "\nC3=" + C3 + "\nC5=" + C5 + "\nC7=" + C7);;
 
         int sum = 0;
-        for(int i=a; i < n; i++){
-            for(int j=b; j < m; j++){
+        for (int i = a; i < n; i++) {
+            for (int j = b; j < m; j++) {
                 int dividend = i - j;
                 int divisor = i - C;
-                if(divisor == 0){
+                if (divisor == 0) {
                     throw new ArithmeticException("Division by zero");
                 } else {
                     int quotient = dividend / divisor;
